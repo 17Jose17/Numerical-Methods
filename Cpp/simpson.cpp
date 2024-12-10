3ed26dc19ff77ec2fce2dff56b7df409413b54f1
Jose_17 <126434408+17Jose17@users.noreply.github.com>
2024-12-10 00:30:01 -0600

    Create simpson.cpp

---
+++ Cpp/simpson.cpp
@@ -0,0 +1,9 @@
+ld simp(ld a, ld b, vector<ld> v){
+    ld h = (b - a) / maxn;
+    ld s = evaluate(v, a) + evaluate(v, b);
+    for(int i = 1; i <= maxn - 1; i++){
+        ld h1 = a + h * i;
+        s += evaluate(v, h1) * ((i & 1) ? 4 : 2);
+    }
+    return s * h / 3.0;
+}
