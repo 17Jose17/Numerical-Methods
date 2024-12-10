9a19a89b42d37d192d8fd5c2ffa8246666998a9f
Jose_17 <126434408+17Jose17@users.noreply.github.com>
2024-12-10 00:26:16 -0600

    Update common.h

--- Cpp/common.h
+++ Cpp/common.h
@@ -11,12 +11,27 @@ using namespace std;
 #define pb push_back
 #define all(v) v.begin(), v.end()
 
+using ld = long double;
+
 const int Inf = 1e9;
 const ll mod = 1e9 + 7;
 const ll INF = 1e18;
 const int maxn = 1e3;
+const ld eps = 1e-9;
+const inf = numeric_limits<ld>::max();
+const pi = acos(-1);
 
-using ld = long double;
+bool geq(ld a, ld b);
+
+bool leq(ld a, ld b);
+
+bool ge(ld a, ld b);
+
+bool le(ld a, ld b);
+
+bool eq(ld a, ld b);
+
+bool neq(ld a, ld b);
 
 ld binaryExponentiation(ld b, ll p);
 
