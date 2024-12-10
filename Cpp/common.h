dc5b8e9511996c99bde13aafd9f136b8c132c5ce
Jose_17 <126434408+17Jose17@users.noreply.github.com>
2024-12-10 00:14:05 -0600

    Create common.h

---
+++ Cpp/common.h
@@ -0,0 +1,27 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#define ll long long int
+#define fi first
+#define se second
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+const int Inf = 1e9;
+const ll mod = 1e9 + 7;
+const ll INF = 1e18;
+const int maxn = 1e3;
+
+using ld = long double;
+
+ld evaluate(vector<ld> v, ld x);
+
+ld binaryExponentiation(ld b, ll p);
+
+vector<ld> derivative(vector<ld> v);
+
+#endif
