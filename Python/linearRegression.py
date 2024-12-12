95604e30dbb60bb0947a96501cc9c4eafb94e757
Jose_17 <126434408+17Jose17@users.noreply.github.com>
2024-12-12 13:49:30 -0600

    Create linearRegression.py

---
+++ Python/linearRegression.py
@@ -0,0 +1,10 @@
+def linear_regression(v, u):
+    v = np.array(v)
+    u = np.array(u)
+    n = len(v)
+    sx = np.sum(v)
+    sy = np.sum(u)
+    sxy = np.sum(v * u)
+    sxx = np.sum(v * v)
+    m = (n * sxy - sx * sy) / (n * sxx - sx * sx)
+    return m, (sy - sx * m) / n
